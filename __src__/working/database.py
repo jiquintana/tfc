3737274df8ca7448e1a584b8bc315260b55197f3
José Ignacio <joseignacio.quintana@gmail.com>
2013-10-03 18:18:22 +0200

    pepe

--- __src__/working/database.py
+++ __src__/working/database.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # vim: ts=4:sw=4:sts=4:ai:et:fileencoding=utf-8:number
+
+import sqlite3 as lite
+import sys
+
+configdb = "database/config.db"
+
+dir = os.path.dirname(filename)
+
+
+
+
+try:
+    os.stat(dir)
+except:
+    os.mkdir(dir)
+    
