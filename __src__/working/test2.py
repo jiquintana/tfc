db9480125b4023ee94634cd4bd00e7fadba2d6d3
José Ignacio <joseignacio.quintana@gmail.com>
2013-10-05 13:11:57 +0200

    na

--- __src__/working/test2.py
+++ __src__/working/test2.py
@@ -5,48 +5,48 @@
 #from SocketServer import ThreadingMixIn
 #from Queue import Queue
 #import threading, socket
-import socket, time
+import socket, time, signal
 from pprint import pprint
 from ThreadPool import ThreadPoolMixIn
 from Proxy import Proxy
-
-#socket.setdefaulttimeout(3)
-
 from SimpleHTTPServer import SimpleHTTPRequestHandler
-#from BaseHTTPServer import BaseHTTPRequestHandler
-#import urlparse
+from SocketServer import TCPServer
+
+class ThreadedServer(ThreadPoolMixIn, TCPServer):
+    numThreads=5
+    pass
+        
+
+
+def test(HandlerClass=Proxy,
+         ServerClass=ThreadedServer,
+         protocol="HTTP/1.0",
+         server_version="blah"):
+    '''
+    Test: Run an HTTP server on port 8002
+    '''
+
+    port = 8002
+    server_address = ('', port)
+
+    HandlerClass.protocol_version = protocol
+    httpd = ServerClass(server_address, HandlerClass)
+
+    sa = httpd.socket.getsockname()
+    #print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    print time.asctime(), "ServerHTTP started on", sa[0], "port", sa[1], "..."
+    httpd.serve_forever()
+    
+    def done():
+        ThreadedServer.shutdown(self)
+        
+
+def Exit_gracefully(signal, frame):
+    test.done()
+    print time.asctime(), "Server stop"
+    sys.exit(0)
 
 if __name__ == '__main__':
-    from SocketServer import TCPServer
-
-    class ThreadedServer(ThreadPoolMixIn, TCPServer):
-	numThreads=5
-        pass
-
-    #def test(HandlerClass=SimpleHTTPRequestHandler,
-    def test(HandlerClass=Proxy,
-            ServerClass=ThreadedServer,
-            protocol="HTTP/1.0",
-            server_version="blah"):
-        '''
-        Test: Run an HTTP server on port 8002
-        '''
-
-        port = 8002
-        server_address = ('', port)
-
-        HandlerClass.protocol_version = protocol
-        httpd = ServerClass(server_address, HandlerClass)
-
-        sa = httpd.socket.getsockname()
-        #print "Serving HTTP on", sa[0], "port", sa[1], "..."
-	print time.asctime(), "ServerHTTP started on", sa[0], "port", sa[1], "..."
-        httpd.serve_forever()
-
-    try:
-	test()
-    except KeyboardInterrupt:
-	pass
-
-
-	print time.asctime(), "Server stop"
+    signal.signal(signal.SIGINT, Exit_gracefully)
+    test()
+        
